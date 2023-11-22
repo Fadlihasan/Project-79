d0da73b2016ef9b1b07284cd637a9793b79f7091
Fadlihasan Syamlithani <fadlisyamlithani@gmail.com>
2023-11-22 13:45:20 +0700

    nih update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,18 +23,11 @@
   .popup {
     transition: all 5s ease-in-out;
     position: relative;
-} 
-
-
-
-
-      .txt {
-            display: none;
-        }
+}      
+.txt {display: none;}
         br {
    display: block;
-   margin: 10px 0;
-}
+   margin: 10px 0;}
 </style>
 </head>
 <body>
@@ -88,8 +81,8 @@
               <p class="text-gray-400 mt-4 text-xs">Please sign in first to explore futher on our website</p>
               <input class="w-full mt-5 rounded-xl border p-2 mb-3" type="email" name="email" placeholder="Email" />
                <div class="relative">
-              <input class="w-full rounded-xl border p-2" type="password" name="password" placeholder="Password" />
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-eye absolute right-3 top-1/2 -translate-y-1/2" viewBox="0 0 16 16">
+              <input class="w-full rounded-xl border p-2" type="password" name="password" id="password-field" placeholder="Password" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" toggle="#password-field" class="fa fa-eye absolute right-3 top-1/2 -translate-y-1/2 field-icon toggle-password" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
               </svg>
@@ -102,8 +95,8 @@
                 <hr class="border-gray-400" />
               </div>
   
-              <button class="mt-5 flex w-full items-center justify-center rounded-xl border-2 bg-white py-2 text-sm text-[#002D74]">
-                <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+              <button class="mt-5 flex w-full items-center justify-center rounded-xl border-2 bg-white py-2 text-sm text-grey3">
+                <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px" fill="#848484">
                   <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
                   <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
                   <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
@@ -158,6 +151,18 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script>
+       
+      $(".toggle-password").click(function() {
+      $(this).toggleClass("fa-eye fa-eye-slash");
+      var input = $($(this).attr("toggle"));
+      if (input.attr("type") == "password") {
+      input.attr("type", "text");
+      } 
+      else {
+      input.attr("type", "password");
+      }
+      });
+      
       $(document).ready(function () {
           var lines = $(".txt");
           var i = -1;
